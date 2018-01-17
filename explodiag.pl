2322c3885ac6e5e895d2db70e3dcec31725b3cbf
zhmylove <zhmylove@narod.ru>
2018-01-18 01:32:52 +0300

    Add handsome output formats

--- explodiag.pl
+++ explodiag.pl
@@ -7,15 +7,21 @@ use v5.8;
 
 my $config_file = './explodiag.conf';
 my $modules_dir = '~/explodiag/modules';
+my $_DEBUG      = 1;
 
 my $USAGE = "$0 <explorer.tar.gz ...>";
 
 our %conf;
-my $_DEBUG = 1;
+my @HOSTS;
+my %result;
+my %modules;
 
-sub debug {
-   print STDERR @_, "\n" if $_DEBUG;
-}
+#TODO
+# force rm / interactive rm and avoid bumblebee issue #123
+# gtar/tar autodetect
+
+# Print debug output
+sub debug { print STDERR @_, "\n" if $_DEBUG; }
 
 # Parse config file
 unless (my $rc = do $config_file) {
@@ -26,11 +32,12 @@ unless (my $rc = do $config_file) {
 
 # Set default values
 my %default_conf = (
-   PREFIX => "/tmp",
-   DEST => "explodiag",
-   TAR => "gtar",
+   PREFIX  => "/tmp",
+   DEST    => "explodiag",
+   TAR     => "gtar",
    CLEANUP => 0,
-   RM_ARGS => "-rf"
+   RM_ARGS => "-rf",
+   OUTPUT  => "text",
 );
 defined $conf{$_} or $conf{$_} = $default_conf{$_} for keys %default_conf;
 
@@ -53,9 +60,7 @@ for (@ARGV) {
 chdir "$DIR" or die $!;
 
 # Rename extracted files to hostnames and fill @HOSTS
-my @HOSTS;
 for (@ARGV) {
-# explorer.856eb306.db53-spb-2012.03.14.11.28.tar.gz
    s/.tar.gz$//;
    (my $to = $_) =~ s/^(?:[^.]*\.){2}(.*)-\d{4}\.\d\d\.\d\d\.\d\d\.\d\d$/$1/;
    debug "Renaming $_ to $to";
@@ -67,7 +72,32 @@ for (@ARGV) {
 exit 0 unless @HOSTS;
 for my $module (sort <$modules_dir/*>) {
    debug "Running $module...";
-   print `"$module" @HOSTS`
+   for (split /\n/, `"$module" @HOSTS`) {
+      my ($host, $code, $text) = split /\//;
+      my $module_name = (split /\//, $module)[-1];
+      $modules{$module_name} = 1;
+      push @{ $result{$host}->{$module_name} }, [ $code, $text ];
+   }
+}
+
+# Format output
+if ($conf{OUTPUT} =~ /^te?xt$/i) {
+   for my $module_name (sort keys %modules) {
+      print " === $module_name === \n";
+      local $" = " | ";
+      for (sort keys %result) {
+         printf "%16s\n", $_;
+         print " " x 16 . "@$_" . "\n" for @{ $result{$_}->{$module_name} };
+      }
+   }
+} elsif ($conf{OUTPUT} =~ /^dump$/i) {
+   require Data::Dumper;
+   local $Data::Dumper::Purity = 1;
+   print Data::Dumper->Dump([\%result], ["explodiag"]);
+} elsif ($conf{OUTPUT} =~ /^html?$/i) {
+   # TODO
+} else {
+   die "Unknown output format: $conf{OUTPUT}\n";
 }
 
 END {
